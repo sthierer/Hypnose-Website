830efdd76421ee9adf68acee75eabde8d5205dff
sthierer <stefffen.thierer@hotmail.com>
2026-02-02 21:09:15 +0100

    fix: debugging

--- scripts/index.js
+++ scripts/index.js
@@ -24,35 +24,44 @@ function isNavOpen() {
 
 // Toggle per Button
 toggle.addEventListener('click', (e) => {
+  console.log("Toggle per Button: "+e)
   const willOpen = !isNavOpen();
   if (willOpen) {
     openNav();
+    console.log("openNav")
   } else {
     closeNav();
+    console.log("closeNav")
   }
 });
 
 // 1) Beim Klick auf einen Nav-Link: schließen
 nav.addEventListener('click', (e) => {
   const link = e.target.closest('a');
+  console.log("click: "+e)
   if (link) {
     // Nur im mobilen Zustand schließen (optional, aber sinnvoll)
     if (window.innerWidth < 1024) {
       closeNav();
+      console.log("closeNav")
     }
   }
 });
 
 // 2) Klick außerhalb von Header/Nav: schließen
 document.addEventListener('click', (e) => {
+  console.log("Klick außerhalb von Header/Nav: schließen: "+e)
+  console.log("header: "+e.target)
   // Wenn Nav offen ist und der Klick NICHT im Header/Toggler stattfindet → schließen
   if (isNavOpen() && !header.contains(e.target)) {
     closeNav();
+     console.log("closeNav")
   }
 });
 
 // 3) ESC zum Schließen
 document.addEventListener('keydown', (e) => {
+  console.log("ESC zum Schließen: "+e)
   if (e.key === 'Escape' && isNavOpen()) {
     closeNav();
     // Fokus zurück auf Toggle, damit Tastaturnutzer:innen nicht „verloren“ sind
